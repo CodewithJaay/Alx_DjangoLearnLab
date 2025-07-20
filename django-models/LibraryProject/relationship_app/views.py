7a7e8856a594d02f0576587fdc42e94afe011f1c
Jackline <jacqyridants@gmail.com>
2025-07-20 19:20:48 +0300

    Fixed list_books view to render template instead of plain text

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
-    output = '\n'.join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(output, content_type="text/plain")
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 
 from django.views.generic.detail import DetailView
 from .models import Library
