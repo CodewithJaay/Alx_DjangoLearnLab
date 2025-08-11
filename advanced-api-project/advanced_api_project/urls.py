53b53c62717e35def4e161b4b9aa0a6a4429c14e
Jackline <jacqyridants@gmail.com>
2025-08-11 20:48:41 +0300

    Include api.urls in project URL configuration

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -15,13 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
@@ -36,5 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),         # Registers /api/authors/ and /api/books/
     path('api-token-auth/', obtain_auth_token), # Token authentication endpoint
+    path('', include('api.urls')),              # Include CRUD views from api/urls.py
 ]
-
