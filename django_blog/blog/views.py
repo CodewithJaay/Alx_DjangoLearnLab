31163378c11787fd5abaacbb6545ae9392e753dc
Jackline <jacqyridants@gmail.com>
2025-08-15 22:03:04 +0300

    Add login required import and update views for permissions

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -6,6 +6,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 
 from .models import Post
