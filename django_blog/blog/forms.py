af9417f3323198d5cab36d9ed73d2edd7c9c8635
Jackline <jacqyridants@gmail.com>
2025-08-17 21:43:43 +0300

    Use TagWidget in PostForm for tag input

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
-from taggit.models import Tag
+from taggit.forms import TagWidget
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -21,12 +21,6 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(
-        required=False,
-        help_text="Add tags separated by commas",
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "e.g. Django, Python, WebDev"})
-    )
-
     class Meta:
         model = Post
         fields = ["title", "content", "tags"]
@@ -34,20 +28,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter post title"}),
             "content": forms.Textarea(attrs={"class": "form-control", "placeholder": "Write your content here..."}),
+            "tags": TagWidget(attrs={"class": "form-control", "placeholder": "e.g. Django, Python, WebDev"}),
         }
-
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        if commit:
-            instance.save()
-        # Handle tags
-        tags_str = self.cleaned_data.get("tags", "")
-        if tags_str:
-            tag_names = [tag.strip() for tag in tags_str.split(",") if tag.strip()]
-            for tag_name in tag_names:
-                tag, created = Tag.objects.get_or_create(name=tag_name)
-                instance.tags.add(tag)
-        return instance
 
 
 class CommentForm(forms.ModelForm):
