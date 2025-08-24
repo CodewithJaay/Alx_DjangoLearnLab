575206f7dbb6775338cdc8a095a5e7df50882233
Jackline <jacqyridants@gmail.com>
2025-08-24 20:31:06 +0300

    Fix serializers: add CharField and get_user_model().objects.create_user for registration

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,6 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -17,7 +18,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
